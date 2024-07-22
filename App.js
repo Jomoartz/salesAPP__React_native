5e060ac04eb37c90eedbca12b2d13c555454ebd7
jomoartz <johnjohananmogbolu@gmail.com>
2024-07-22 04:22:40 +0100

    updates

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
-import Screen from "./app/components/Screen";
-import colors from "./app/config/colors";
-import React, { useEffect, Alert, useState } from "react";
-import FormScreen from "./app/screens/FormScreen";
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import RegisterScreen from './app/screens/RegisterScreen';
+import LoginScreen from './app/screens/LoginScreen'
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
+const Stack = createNativeStackNavigator();
 
+const AuthNavigator= () => {
+  return (<NavigationContainer>
+  <Stack.Navigator initialRouteName='Welcome'>
+  <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    <Stack.Screen name="Register" component={RegisterScreen} />
+    <Stack.Screen name="Login" component={LoginScreen} />
+  </Stack.Navigator>
+</NavigationContainer>
+);}
 
 export default function App() {
 
-
+  
   return (
-  <FormScreen/>
+    <AuthNavigator/>
 )}
+
+     
+
+
 
 
